a08b04c8b5ad2dc2cbfb37f0167ab9f4fd676e48
riversnails <ekgur120736@gmail.com>
2020-10-14 21:01:17 +0900

    add

--- main/ledstrip.cpp
+++ main/ledstrip.cpp
@@ -1,8 +1,6 @@
+#include "ledstrip.h"
 #include <FastLED.h>   // 온라인으로 다운바람
 
-#define LED_PIN     8  // 스트립 핀번호
-#define NUM_LEDS    5 // 스트립 길이
-#define BRIGHTNESS  64 // 밝기
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 CRGB leds[255];
@@ -10,20 +8,38 @@ CRGB leds[255];
 #define UPDATES_PER_SECOND 100
 
 CRGBPalette16 currentPalette;
+CRGBPalette16 usepalette;
 TBlendType    currentBlending;
 
 extern CRGBPalette16 myRedWhiteBluePalette;
 extern const TProgmemPalette16 myRedWhiteBluePalette_p PROGMEM;
 
 
-void FillLEDsFromPaletteColors(uint8_t colorIndex) // 컬러 집어넣기? 아무튼 필요함
+void FillLEDsFromPaletteColors(uint8_t colorIndex) // 컬러 집어넣기
 {
   uint8_t brightness = 255;
 
   for (int i = 0; i < 255; i++) {
     if(i == 2) 
     {
-      leds[i] = ColorFromPalette(currentPalette, colorIndex, brightness, currentBlending);
+      fill_solid(usepalette, 16, CRGB::White);
+      leds[i] = ColorFromPalette(usepalette, colorIndex, brightness, currentBlending);
+      continue;
+    }
+    leds[i] = ColorFromPalette(currentPalette, colorIndex, brightness, currentBlending);
+    colorIndex += 1;
+  }
+}
+
+void FillLEDsFromPaletteColors(uint8_t colorIndex, char *arr[]) // 컬러 집어넣기
+{
+  uint8_t brightness = 255;
+
+  for (int i = 0; i < 255; i++) {
+    if(arr[i] == 1)
+    {
+      fill_solid(usepalette, 16, CRGB::White);
+      leds[i] = ColorFromPalette(usepalette, colorIndex, brightness, currentBlending);
       continue;
     }
     leds[i] = ColorFromPalette(currentPalette, colorIndex, brightness, currentBlending);
@@ -36,6 +52,11 @@ void SetupAllColor(CRGB color) // 한 컬러로 다 넣기
   fill_solid(currentPalette, 16, color);
 //    currentPalette[0] = CRGB::White;
 //    currentPalette[12] = CRGB::White;
+}
+
+void led_push_location_color(CRGB in_color, char *arr[])
+{
+  
 }
 
 void led_push_all_color(CRGB in_color) // 하나의 컬러만 다 넣어버리기
--- main/ledstrip.h
+++ main/ledstrip.h
@@ -4,8 +4,13 @@
 #include "Arduino.h"
 #include <FastLED.h>
 
+#define LED_PIN     8  // 스트립 핀번호
+#define NUM_LEDS    20 // 스트립 길이
+#define BRIGHTNESS  20 // 밝기
+
 void FillLEDsFromPaletteColors(uint8_t colorIndex);
 void SetupAllColor(CRGB color);
+void led_push_location_color(CRGB in_color, char *arr[]);
 void led_push_all_color(CRGB in_color);
 void led_init();
 #endif
